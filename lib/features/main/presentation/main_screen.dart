aecb64e0c3bf9d834faffc6760385b7bb1156412
CowboyGH <kocergindenis81@gmail.com>
2025-05-27 14:11:22 +0700

    add: implement total water intake with a progress indicator and goal information
    - Changed the body of the main screen to use SingleChildScrollView for better scrolling experience
    - Added a SafeArea and Padding for improved UI layout
    - Enhanced the display of total water intake with a progress indicator and goal information
    - Updated the structure of the main screen to improve readability and maintainability
    
    This commit enhances the user interface and experience of the main screen by providing a more organized layout and better visual feedback on water intake goals.

--- lib/features/main/presentation/main_screen.dart
+++ lib/features/main/presentation/main_screen.dart
@@ -49,42 +49,120 @@ class _MainScreenState extends State<MainScreen> {
           textAlign: TextAlign.center,
         ),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Container(
-              height: size.height * 0.2,
-              width: size.width * 0.5,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surface,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: theme.colorScheme.shadow.withOpacity(0.1),
-                    blurRadius: 8,
-                    offset: const Offset(0, 4),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Container(
+                  height: size.height * 0.125,
+                  width: size.width,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: theme.colorScheme.shadow.withOpacity(0.1),
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: PageView.builder(
-                itemCount: waterAmounts.length,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => GestureDetector(
-                    onTap: () => _onWaterAmountTap(index),
-                    child: Container(
-                        color: Colors.transparent,
-                        child: WaterVariant(waterAmount: waterAmounts[index]))),
-              ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total',
+                            style: theme.textTheme.labelMedium?.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                          Text(
+                            '$totalAmount ml',
+                            style: theme.textTheme.labelLarge?.copyWith(
+                              color: theme.colorScheme.primary,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            'Goal: 2000 ml',
+                            style: theme.textTheme.labelSmall?.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: 60,
+                            height: 60,
+                            child: CircularProgressIndicator(
+                              value: totalAmount / 2000,
+                              color: theme.colorScheme.primary,
+                              strokeWidth: 8,
+                              backgroundColor:
+                                  theme.colorScheme.primary.withOpacity(0.2),
+                            ),
+                          ),
+                          Text(
+                            '${((totalAmount / 2000) * 100).toInt()}%',
+                            style: theme.textTheme.labelLarge?.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    height: size.height * 0.2,
+                    width: size.width * 0.5,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.surface,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: theme.colorScheme.shadow.withOpacity(0.1),
+                          blurRadius: 8,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: PageView.builder(
+                      itemCount: waterAmounts.length,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) => GestureDetector(
+                          onTap: () => _onWaterAmountTap(index),
+                          child: Container(
+                              color: Colors.transparent,
+                              child: WaterVariant(
+                                  waterAmount: waterAmounts[index]))),
+                    ),
+                  ),
+                ),
+                Text(
+                  '$totalAmount ml',
+                  style: theme.textTheme.labelLarge?.copyWith(
+                    color: theme.colorScheme.primary,
+                  ),
+                )
+              ],
             ),
           ),
-          Text(
-            '$totalAmount ml',
-            style: theme.textTheme.labelLarge?.copyWith(
-              color: theme.colorScheme.primary,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
