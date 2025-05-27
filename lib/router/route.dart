58923e730ef84a76177d14a7ff29c3d404809ca0
CowboyGH <kocergindenis81@gmail.com>
2025-05-27 11:04:50 +0700

    add: implement main screen to the app

--- lib/router/route.dart
+++ lib/router/route.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:water_tracker/features/main_screen.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => Scaffold()),
+    GoRoute(path: '/', builder: (context, state) => const MainScreen()),
   ],
 );
